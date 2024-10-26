00e11110853ccf20aebfe25affadaada168fe256
victoriastruk <struk.victoria@gmail.com>
2024-10-26 13:49:09 +0300

    Add shredCat

--- assets/js/promiscification.js
+++ assets/js/promiscification.js
@@ -19,4 +19,18 @@ promise
 //   console.log("data :>> ", data)
 // );
 // Promise.reject().catch();
-console.log('end of sync code');
+console.log("end of sync code");
+
+// проміс - кіт Шредингера
+const executor = function (resolve, reject) {
+  if (Math.random() < 0.5) {
+    resolve("cat is alive");
+  } else {
+    reject(new Error("cat is not alive"));
+  }
+};
+const shredCat = new Promise(executor);
+
+shredCat
+  .then((data) => console.log("data :>> ", data))
+  .catch((err) => console.log("err :>> ", err));
